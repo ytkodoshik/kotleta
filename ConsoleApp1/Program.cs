792642f54d85c265ba5f15e9722451c576577c30
Лесик Юлия <ytkodoshik@gmail.com>
2023-06-26 09:33:09 +0200

    kek

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var x = 2;
+var y = 3;
+
+Console.WriteLine(-6 * x ^ 3 + 5 * x ^ 2 - 10 * x + 15);
+Console.WriteLine(Math.Abs(x) * Math.Sin(x));
+Console.WriteLine(2 * Math.PI * x);
+Console.WriteLine(Math.Max(x, y));
+
+byte a = 5;
+int c = 8;
+short d = 9;
+double g = 10;
+Console.WriteLine(a + c);
+Console.WriteLine(g - d);
+Console.WriteLine(c * d);
